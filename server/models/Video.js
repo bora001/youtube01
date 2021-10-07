335b7dc3827e30b3ceea4000e506de979c59ef56
bora001 <bora.vancouver@gmail.com>
2021-10-07 14:27:02 -0700

    videoSchema

---
+++ server/models/Video.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+const videoSchema = mongoose.Schema({
+    writer: {
+        type: Schema.Types.ObjectId,
+        //to get Userinfo from the UserSchema
+        ref:'User'
+    },
+    title: {
+        type: String,
+        maxlength:50
+    },
+    description: {
+        type: String
+    },
+    privacy: {
+        type: Number
+        //0 true, 1 false
+    },
+    filePath: {
+        type:String
+    },
+    category: {
+        type:String
+    },
+    views: {
+        type: Number,
+        default: 0
+    },
+    duration: {
+        type:String
+    },
+    thumbnail: {
+        type:String 
+    }
+
+}, {timestamps:true})
+
+
+const Video = mongoose.model('Video', videoSchema)
+module.exports = { Video }
