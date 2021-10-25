1112a4b914f0de43a9ef1a21b933c94431cc04c2
bora001 <bora.vancouver@gmail.com>
2021-10-25 14:27:02 -0700

    comment input

--- client/src/components/views/VideoDetailPage/Section/Comment.js
+++ client/src/components/views/VideoDetailPage/Section/Comment.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 function Comment() {
+
+    const [InputValue, setInputValue] = useState('')
+    
+    const InputComment = (event) => {
+        setInputValue(event.target.value)
+    }
+
+    const onComment = (event) => {
+        event.preventDefault()
+
+        const variable = {
+            comment : InputValue,
+        }
+        axios.post('/api/comment/saveComment', variable)
+            .then(response => {
+                if (response.data.success) {
+                
+                } else {
+                    alert("failed to save your comment, try again")
+            }
+        })
+        console.log(InputValue)
+    }
+
     return (
         <div>
-            Comment
+            <form onSubmit={onComment}style={{ display:'flex', flexDirection:'row',justifyContent:'flex-start',alignItems:'flex-start', width:'100%',height:'50px'}}>
+                <input onChange={InputComment}/>
+            <button onClick={onComment}>submit</button>
+            </form>
         </div>
     )
 }
